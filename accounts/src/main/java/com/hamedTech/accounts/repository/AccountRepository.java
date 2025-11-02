13697ff2f328c1c65f3a366d51052e6fab971668
hamedmiri <hamedmiri72@gmail.com>
2025-11-02 18:45:53 +0000

    loans microservice is completed

--- accounts/src/main/java/com/hamedTech/accounts/repository/AccountRepository.java
+++ accounts/src/main/java/com/hamedTech/accounts/repository/AccountRepository.java
@@ -1,7 +1,9 @@
 package com.hamedTech.accounts.repository;
 
 import com.hamedTech.accounts.entity.Accounts;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,5 +13,7 @@ public interface AccountRepository extends JpaRepository<Accounts, Long> {
 
     Optional<Accounts> findByCustomerId(Long customerId);
 
+    @Transactional
+    @Modifying
     void deleteByCustomerId(Long customerId);
 }
