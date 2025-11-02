c00d3e23a7e9c2ba9ae47d63b31b516e0b7c8fb6
hamedmiri <hamedmiri72@gmail.com>
2025-11-02 17:25:28 +0000

    exception handling

--- loans/src/main/java/com/hamedTech/loans/exception/GlobalExceptionHandler.java
+++ loans/src/main/java/com/hamedTech/loans/exception/GlobalExceptionHandler.java
@@ -1,17 +1,25 @@
 package com.hamedTech.loans.exception;
 
 import com.hamedTech.loans.dto.ErrorResponseDto;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.method.MethodValidationException;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 
     private final WebRequest webRequest;
@@ -19,6 +27,24 @@ public class GlobalExceptionHandler {
     public GlobalExceptionHandler(WebRequest webRequest) {
         this.webRequest = webRequest;
     }
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        Map<String, String> validationError = new HashMap<>();
+
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+
+        validationErrorList.forEach(error -> {
+            String fieldName = ((FieldError)error).getField();
+            String errorMessage = error.getDefaultMessage();
+            validationError.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(validationError, HttpStatus.BAD_REQUEST);
+    }
+
 
     @ExceptionHandler(LoansAlreadyExists.class)
     public ResponseEntity<ErrorResponseDto> handleLoansAlreadyExist(WebRequest webRequest, LoansAlreadyExists exception){
